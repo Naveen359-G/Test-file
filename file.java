b957e32290cf1d4b42342bb35034703fd5c7cb21
Naveen359-G <naveen.gajavelli359@gmail.com>
2020-09-03 20:47:55 +0530

    java program to print pascal triangle

--- file.java
+++ file.java
@@ -1,20 +1,35 @@
-class GetMyIPAddress
-{
-   public static void main(String args[]) throws Exception
-   {
-      /* public static InetAddress getLocalHost()
-       * throws UnknownHostException: Returns the address 
-       * of the local host. This is achieved by retrieving 
-       * the name of the host from the system, then resolving 
-       * that name into an InetAddress. Note: The resolved 
-       * address may be cached for a short period of time.
-       */
-      InetAddress myIP=InetAddress.getLocalHost();
- 
-      /* public String getHostAddress(): Returns the IP 
-       * address string in textual presentation.
-       */
-      System.out.println("My IP Address is:");
-      System.out.println(myIP.getHostAddress());
-  }
+package com.beginnersbook;
+import java.util.Scanner;
+public class JavaExample {
+    static int fact(int num) {
+	int factorial;
+
+	for(factorial = 1; num > 1; num--){
+		factorial *= num;
+	}
+	return factorial;
+    }
+    static int ncr(int n,int r) {
+	return fact(n) / ( fact(n-r) * fact(r) );
+    }
+    public static void main(String args[]){
+	int rows, i, j;
+
+	//getting number of rows from user
+	System.out.println("Enter number of rows:");
+	Scanner scanner = new Scanner(System.in);
+	rows = scanner.nextInt();
+	scanner.close();
+
+	System.out.println("Pascal Triangle:");
+	for(i = 0; i < rows; i++) {
+		for(j = 0; j < rows-i; j++){
+			System.out.print(" ");
+		}
+		for(j = 0; j <= i; j++){
+			System.out.print(" "+ncr(i, j));
+		}
+		System.out.println();
+ 	}
+    }
 }
