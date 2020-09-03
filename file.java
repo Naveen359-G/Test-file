4fb806cf0c2d96254998ee277c2187e710541d2e
Naveen359-G <naveen.gajavelli359@gmail.com>
2020-09-03 20:40:18 +0530

    modified java file

--- file.java
+++ file.java
@@ -1,6 +1,20 @@
-public class Hello {
-public static void main (string {} args)
-//prints "Hello World" in the terminal window,
-system.out.printfln ("Hello World");
-}
+class GetMyIPAddress
+{
+   public static void main(String args[]) throws Exception
+   {
+      /* public static InetAddress getLocalHost()
+       * throws UnknownHostException: Returns the address 
+       * of the local host. This is achieved by retrieving 
+       * the name of the host from the system, then resolving 
+       * that name into an InetAddress. Note: The resolved 
+       * address may be cached for a short period of time.
+       */
+      InetAddress myIP=InetAddress.getLocalHost();
+ 
+      /* public String getHostAddress(): Returns the IP 
+       * address string in textual presentation.
+       */
+      System.out.println("My IP Address is:");
+      System.out.println(myIP.getHostAddress());
+  }
 }
